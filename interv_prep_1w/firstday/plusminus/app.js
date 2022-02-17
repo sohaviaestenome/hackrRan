4fecfbef2638595c3011754c7a4bb9ebae12df6c
Pedro Silva <pacs@outlook.pt>
2022-02-17 16:09:47 +0000

    finished first

--- interv_prep_1w/firstday/plusminus/app.js
+++ interv_prep_1w/firstday/plusminus/app.js
@@ -1,9 +1,31 @@
-const array = [1,-2,-3,-5,-8,5,7];
+const array = [1,-2,-3,-5,-8,5,7,0, 0];
+let pos = 0;
+let neg = 0;
+let zer = 0;
+
 
 
 function plusMinus(arr) {
   const len = arr.length;
-  console.log(len);
+  let pos = 0;
+  let neg = 0;
+  let zer = 0;
+  arr.forEach(value => {
+   let signal =  Math.sign(value);
+
+
+    if (signal === 1) {
+      pos +=1;
+    } else if (signal === -1) {
+      neg += 1;
+    } else if ( signal === 0) {
+      zer +=1;
+    }
+  });
+  console.log(`${(pos/(len)).toFixed(6)}`);
+  console.log(`${(neg / (len)).toFixed(6) }`);
+  console.log(`${(zer / (len)).toFixed(6) }`);
+
 
 }
 
