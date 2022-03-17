ebf8e639c55bd33481cbea852b63ae691e68c400
pacs <pacs@outlook.pt>
2022-03-17 13:21:59 +0000

    tinkered

--- interv_prep_1w/day5/mergeLinkedL/app.js
+++ interv_prep_1w/day5/mergeLinkedL/app.js
@@ -1,12 +1,12 @@
 function mergeLists(head1, head2) {
     const response = [];
     while (head1) {
-        response.push(head1.data)
-        head1 = head1.next
+        response.push(head1.data);
+        head1 = head1.next;
     }
     while (head2) {
-        response.push(head2.data)
-        head2 = head2.next
+        response.push(head2.data);
+        head2 = head2.next;
     }
     return {
         data: response.sort((x, y) => x - y).join(' '),
